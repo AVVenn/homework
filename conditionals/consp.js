a5d3b2bf6f8e4cb5b69945d07f59770591689167
VeN <m6365813@mail.ru>
2021-10-03 11:18:53 +0300

    switch and ?

---
+++ conditionals/consp.js
@@ -0,0 +1,53 @@
+// 1. пользователь вводит номер дня недели
+// вывести название этого дня в консоль
+// если пользователь ввел некорректные данные то вывести 'incorrect data'
+
+            // Решение
+// let randomDay = prompt(`Введите номер дня недели`);
+// switch (randomDay) {
+//     case "1" :
+//         console.log (`Понедельник`);
+//         break;
+//     case "2" :
+//         console.log (`Вторник`);
+//         break;
+//     case "3" :
+//         console.log (`Среда`);
+//         break;
+//     case "4" :
+//         console.log (`Четверг`);
+//         break;
+//     case "5" :
+//         console.log (`Пятница`);
+//         break;
+//     case "6" :
+//         console.log (`Суббота`);
+//         break;
+//     case "7" :
+//         console.log (`Воскресенье`);
+//         break;
+//     default:
+//         console.log (`Неверное значение`);
+// }
+
+// 2. Переписать в тернарный оператор
+// x ? y : z
+// let age = 20;
+
+// if (age < 10) {
+//     console.log('HI!');
+// } else if (age < 20) {
+//     console.log('Hello!');
+// } else {
+//     console.log('Good day!');
+// }
+
+        // Решение
+// let age = 5;
+// console.log (
+// age<10 
+// ? "Hi" 
+// :age<20 
+        //  ? 'Hello' 
+        // : 'Good day!'
+//)
